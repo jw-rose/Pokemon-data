75779ca0d986ec082036374ce52066ff08215d74
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-02-14 13:11:26 +0100

    pagination added

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const pokemonContainer = document.getElementById("container")
+const pokemonContainer = document.getElementById("container");
+const showMore = document.getElementById("show-more");
+const showLess = document.getElementById("show-less");
+let page = 1;
+const postsPerPage = 10;
+const searchInput = document.querySelector(".find-poke")
+let allPokemon =[];
+let isSearching = false;
 
-async function getPokemon() {
+searchInput.addEventListener('input', (e) => {
+    const searchTerm = e.target.value.toLowerCase().trim()
+})
 
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
+
+
+const getPokemon = async () => {
+    pokemonContainer.innerHTML = '';
+    const skip = (page - 1) * postsPerPage;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${skip}`)
     const pokeData = await response.json();
     
 
@@ -19,13 +33,34 @@ async function getPokemon() {
         container.innerHTML = `
         <h1>${poke.name.charAt(0).toUpperCase()}${poke.name.slice(1)} </h1>
         <img class="poke-sprite" src ="${infoData.sprites.front_default}" alt="${poke.name}">
-        
+
         `;
-        
+
     pokemonContainer.appendChild(container);
     }
-
-    
 }
+
+ showMore.addEventListener("click", () => {
+    page++;
+    getPokemon();
+    updateButtons();
+})
+
+showLess.addEventListener("click", () => {
+    if (page > 1) {
+        page--;
+        getPokemon();
+        updateButtons();
+    }
+})
+
+function updateButtons() {
+   
+    showLess.disabled = (page === 1);
+    
+
+}
+
+ 
 
 getPokemon()
