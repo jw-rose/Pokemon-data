c5d8c089082a782c730c66ee91fdd719a8c8c5cc
jw-rose <220035988+jw-rose@users.noreply.github.com>
2026-02-18 15:07:24 +0100

    update

--- script.js
+++ script.js
@@ -1,66 +1,55 @@
-const pokemonContainer = document.getElementById("container");
-const showMore = document.getElementById("show-more");
-const showLess = document.getElementById("show-less");
-let page = 1;
-const postsPerPage = 10;
-const searchInput = document.querySelector(".find-poke")
-let allPokemon =[];
-let isSearching = false;
+const pokeCardTemplate = document.querySelector("[poke-template]")
+const pokeContainer = document.querySelector("[poke-cards-container]")
+const searchInput =document.querySelector("[data-search]")
 
-searchInput.addEventListener('input', (e) => {
-    const searchTerm = e.target.value.toLowerCase().trim()
+let allPokemon = []
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    allPokemon.forEach(pokemon => {
+        const isVisible = pokemon.name.toLowerCase().includes(value)
+        pokemon.element.classList.toggle("hide", !isVisible)
+    })
 })
 
 
+async function getPokemon() {
+const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+const data = await response.json()
 
-const getPokemon = async () => {
-    pokemonContainer.innerHTML = '';
-    const skip = (page - 1) * postsPerPage;
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${skip}`)
-    const pokeData = await response.json();
-    
+const pokemonData = await Promise.all(
+    data.results.map(pokemon => 
+        fetch(pokemon.url).then (res => res.json())
+    )
+)
 
-    for (const poke of pokeData.results) {
-        const infoResponse = await fetch(poke.url)
-        const infoData = await infoResponse.json();
+allPokemon = pokemonData.map(pokemon => {
+    const card = pokeCardTemplate.content.cloneNode(true).children[0]
 
-
-        const container = document.createElement("container")
-        container.className = "poke-card";
-       
-        const content = document.createElement("div")
-        content.className = "poke-text"
-        container.innerHTML = `
-        <h1>${poke.name.charAt(0).toUpperCase()}${poke.name.slice(1)} </h1>
-        <img class="poke-sprite" src ="${infoData.sprites.front_default}" alt="${poke.name}">
-
-        `;
-
-    pokemonContainer.appendChild(container);
+    const name = card.querySelector("[poke-name]")
+    const image = card.querySelector("[poke-image]")
+    const type = card.querySelector("[poke-type]")
+    const height = card.querySelector("[poke-type]")
+    name.textContent = pokemon.name
+    image.src = pokemon.sprites.front_default
+    image.alt = pokemon.name
+    type.textContent = pokemon.type
+    height.textContent = `Height ${pokemon.height}m`
+    pokeContainer.append(card)
+    return {
+        name: pokemon.name,
+        image: pokemon.sprites.front_default,
+        type: pokemon.type,
+        height: pokemon.height,
+        element: card 
     }
-}
 
- showMore.addEventListener("click", () => {
-    page++;
-    getPokemon();
-    updateButtons();
+
 })
-
-showLess.addEventListener("click", () => {
-    if (page > 1) {
-        page--;
-        getPokemon();
-        updateButtons();
-    }
-})
-
-function updateButtons() {
-   
-    showLess.disabled = (page === 1);
-    
 
 }
 
- 
 
 getPokemon()
+
+
